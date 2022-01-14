59b9392f12887621244c952a0fad03d3b76bdb07
aysan <aysanbidariiii@gmail.com>
2022-01-14 04:39:52 +0330

    data-0-added

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.postimg.cc/4NYyMq9g/chatapp.jpg",
+      link: "https://github.com/eyesunbidari-dev/aysan-chat.git",
+    },
+    {
+      id: 2,
+      img: "https://i.postimg.cc/1tR5KjQb/cryptoapp.jpg",
+      link: "https://github.com/eyesunbidari-dev/crypto-app.git",
+    },
+    {
+      id: 3,
+      img: "https://i.postimg.cc/XqpjRBX8/reactredux-todo.jpg",
+      link: "https://github.com/eyesunbidari-dev/reactredux-todo.git",
+    },
+    {
+      id: 4,
+      img: "https://i.postimg.cc/7ZgHrkPp/shop.jpg",
+      link: "https://github.com/eyesunbidari-dev/shop.git",
+    },
+    {
+      id: 5,
+      img: "https://i.postimg.cc/c18mXk21/weatherapp1.jpg",
+      link: "https://github.com/eyesunbidari-dev/weather-app.git",
+    },
+    {
+      id: 6,
+      img: "https://i.postimg.cc/NMPYkGbr/reactform.jpg",
+      link: "https://github.com/eyesunbidari-dev/signup-login-form.git",
+    },
+    
+  ];
